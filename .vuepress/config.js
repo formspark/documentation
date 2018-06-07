44a926b32509116dcf492043a6e61cf50581c541
Bjorn <krols.bj@gmail.com>
2018-06-07 16:48:35 +0200

    Add favicon

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,11 +1,17 @@
 module.exports = {
   title: 'Formspark Documentation',
   description: 'Documentation for Formspark',
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
   themeConfig: {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Formspark', link: 'https://formspark.io' }
     ],
-    sidebar: ['/', 'advanced-features', 'special-input-types', 'form-validation']
+    sidebar: [
+      '/',
+      'advanced-features',
+      'special-input-types',
+      'form-validation'
+    ]
   }
 };
