748be69f3693f7eb8a1f16e3a699572b91e540cb
Bjorn <krols.bj@gmail.com>
2019-05-09 14:29:18 +0200

    Track navigator

--- .vuepress/enhanceApp.js
+++ .vuepress/enhanceApp.js
@@ -5,6 +5,7 @@ export default ({ router }) => {
     const lyticus = new Lyticus("Cxd-1LCX8Lzbe_KSuk0Yy", {
       development: process.env.NODE_ENV === "development"
     });
+    lyticus.trackNavigator();
     router.afterEach(to => {
       lyticus.trackPage(to.fullPath);
     });
