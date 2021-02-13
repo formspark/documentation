85bf4680cae6c42920d6f9e12abe2b4cddaf42a4
Bjorn Krols <krols.bj@gmail.com>
2021-02-13 09:56:30 +0100

    Zapier integration

---
+++ docs/.vuepress/components/ZapierEmbed.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="zapier-widget"/>
+</template>
+
+<script>
+export default {
+  mounted () {
+    const service = 'formspark';
+    const id = 'zapier-widget'
+    const script = document.createElement('script')
+    script.type = 'text/javascript'
+    script.src =
+        `https://zapier.com/apps/embed/widget.js?services=${service}&html_id=${id}`
+    document.head.appendChild(script)
+  }
+}
+</script>
+
+<style scoped>
+#zapier-widget {
+  margin-top: 2rem;
+}
+</style>
--- docs/integration/zapier.md
+++ docs/integration/zapier.md
@@ -8,5 +8,7 @@ lang: en-US
 Connecting Formspark and Zapier takes only seconds.
 
 1. Copy your `Zapier key` found in your form's settings.
-2. Create a Formspark connection in Zapier via [this link](https://zapier.com/developer/public-invite/123998/f5d7e184eeb6d676b872f58195940143/).
+2. Create a Formspark connection in Zapier via [this link](https://zapier.com/apps/formspark/integrations).
 3. Paste your copied key when prompted.
+
+<ZapierEmbed/>
